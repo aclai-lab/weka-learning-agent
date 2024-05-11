a6e7e74a1d348babfc7b05ffdd579a7f11b69f92
mauro-milella <milella_mauro@live.it>
2024-05-11 11:42:37 +0200

    Print di test (per provare i nuovi dataset) rimossa

--- src/Problem.java
+++ src/Problem.java
@@ -38,8 +38,6 @@ public class Problem {
 		m_problemFileName = problemName + (problemName.endsWith(".arff") ? "" : ".arff");
 		m_problemFile = new File(m_problemFileName);
 
-		InputManager.systemMessage(m_problemFileName);
-
 		if (m_problemFile.isFile()) {
 			InputManager.systemMessage("il problema è già presente in memoria");
 			loadProblem();
