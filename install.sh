7abe482ce97569df0a0896bed89c5ec52c701d26
ferdiu <ferdiu.manzella@gmail.com>
2025-06-05 08:45:57 +0200

    smarter install.sh
    Signed-off-by: ferdiu <ferdiu.manzella@gmail.com>

--- install.sh
+++ install.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
+WEKA_VERSION="weka-3-9-6"
+
 # Detect correct OS
 if [[ "$OSTYPE" == "darwin"* ]]; then
     echo -n "Running on macOS "
     if [[ "$(uname -m)" == "arm64" ]]; then
         echo "(arm64)"
-        DOWNLOAD_URL=https://prdownloads.sourceforge.net/weka/weka-3-9-6-azul-zulu-arm-osx.dmg
+        DOWNLOAD_URL=https://prdownloads.sourceforge.net/weka/${WEKA_VERSION}-azul-zulu-arm-osx.dmg
     else
         echo "(amd64)"
-        DOWNLOAD_URL=https://prdownloads.sourceforge.net/weka/weka-3-9-6-azul-zulu-osx.dmg
+        DOWNLOAD_URL=https://prdownloads.sourceforge.net/weka/${WEKA_VERSION}-azul-zulu-osx.dmg
     fi
 else
     echo "Running on Linux"
-    DOWNLOAD_URL="https://prdownloads.sourceforge.net/weka/weka-3-9-6-azul-zulu-linux.zip"
+    DOWNLOAD_URL="https://prdownloads.sourceforge.net/weka/${WEKA_VERSION}-azul-zulu-linux.zip"
 fi
 WEKA_DIR="weka"
 
-if [ ! -d "$WEKA_DIR" ]; then
+if [ ! -d "$WEKA_DIR" ] || \
+   [ ! -f "$WEKA_DIR/$WEKA_VERSION/weka.jar" ] || \
+   [ ! -f "$WEKA_DIR/$WEKA_VERSION/weka-src.jar" ] || \
+   [ ! -f "$WEKA_DIR/$WEKA_VERSION/weka.sh" ]; then
     echo "Downloading Weka..."
     mkdir -p "$WEKA_DIR"
     curl -L -o "$WEKA_DIR/weka.zip" "$DOWNLOAD_URL"
